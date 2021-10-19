82270b4bbd286e82de439074d3e4dd1e3b96c12d
APrzysiuda <180517@umons.ac.be>
2021-10-19 11:42:41 +0200

    Feat: structure test

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,5 +1,17 @@
+import * as monocdk from 'monocdk';
+import {FlywayConstruct} from '../src';
+import {expect, haveResource} from '@monocdk-experiment/assert';
 
-
-test('hello', () => {
-  expect(true).toBe(true);
+test('test stack', () => {
+  const app = new monocdk.App();
+  const stack = new monocdk.Stack(app, 'Test');
+  const bucket = new monocdk.aws_s3.Bucket(stack, 'bucket');
+  const vpc = new monocdk.aws_ec2.Vpc(stack, 'vpc');
+  const secret = new monocdk.aws_secretsmanager.Secret(stack, 'secret');
+  const construct = new FlywayConstruct(stack, 'testConstruct', {
+    migrationDBSecretManager: secret,
+    bucketMigrationSQL: bucket,
+  });
+  expect(construct).to(haveResource('AWS::Lambda::Function'));
+  expect(stack).to(haveResource('AWS::Lambda::Function'));
 });
