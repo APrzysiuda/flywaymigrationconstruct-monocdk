fecf47c2af9bcdc813a4655999574f2fe7dd0822
APrzysiuda <180517@umons.ac.be>
2021-10-19 11:44:32 +0200

    Fix: test

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -6,7 +6,6 @@ test('test stack', () => {
   const app = new monocdk.App();
   const stack = new monocdk.Stack(app, 'Test');
   const bucket = new monocdk.aws_s3.Bucket(stack, 'bucket');
-  const vpc = new monocdk.aws_ec2.Vpc(stack, 'vpc');
   const secret = new monocdk.aws_secretsmanager.Secret(stack, 'secret');
   const construct = new FlywayConstruct(stack, 'testConstruct', {
     migrationDBSecretManager: secret,
